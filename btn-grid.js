fc44ac108b146954cc6ed367c72536ee7e283d10
flkt-crnpio <flkt.crnpio@gmail.com>
2019-12-20 12:58:43 -0600

    btn grid

--- btn-grid.js
+++ btn-grid.js
@@ -1,18 +1,29 @@
 const div = document.createElement('div');
-
+div.id = 'btns';
+const btns = [];
 function addButtons(){
-  for(let x=1; x<=9; x++){
-    let btn = document.createElement('button');
-    btn.innerText = x;
-    btn.className = 'btn';
-    btn.id = 'btn'+x;
-    div.appendChild(btn);
+  for(let i=0; i<9; i++){
+    btns.push(document.createElement('button'));
+    btns[i].innerText = i+1;
+    btns[i].className = 'btn';
+    btns[i].id = 'btn'+(i+1);
+    div.appendChild(btns[i]);
   }
 }
 addButtons();
 document.body.appendChild(div);
 
-const actionBtn = document.getElementById('btn5');
-actionBtn.addEventListener('click', event => {
-  console.log(actionBtn.innerText);
+const orderBtns = [1,2,3,6,9,8,7,4];
+document.getElementById('btn5').addEventListener('click', event => {
+    for(let i=0; i<9; i++){
+      if(i != 4) {
+        let idx = orderBtns.indexOf(parseInt(btns[i].innerText));
+        console.log(idx);
+        if(idx === 0){
+          btns[i].innerText = orderBtns[7];
+        } else {
+          btns[i].innerText = orderBtns[idx - 1];
+        }
+      }
+    }
 });
